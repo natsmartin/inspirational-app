7b0967e0e6623fa42b3eced6300beee743f73da8
natsmartin <107980240+natsmartin@users.noreply.github.com>
2025-04-05 11:06:30 +0800

    update

--- app/features/background/BackgroundImage.jsx
+++ app/features/background/BackgroundImage.jsx
@@ -5,7 +5,7 @@ function BackgroundImage({ background }) {
     <div className="absolute z-[-1] md:w-screen md:h-screen">
       <img
         src={background?.urls.full}
-        className="fixed z-0 blur-xs min-w-max md:min-w-full h-fu"
+        className="fixed z-0 blur-xs min-w-max md:min-w-full"
         id="background-image"
         alt={background?.alt_description}
       ></img>
