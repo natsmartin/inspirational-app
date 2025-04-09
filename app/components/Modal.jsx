061d4f6bbf623cff5b6258ca286be109f586d769
natsmartin <107980240+natsmartin@users.noreply.github.com>
2025-04-09 11:14:55 +0800

    fix modal

--- app/components/Modal.jsx
+++ app/components/Modal.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Modal = ({ onClick, error, display, id }) => {
   return (
     <div id={id} className={`fixed p-10 z-50 bg-[rgba(0,0,0,0.5)] w-screen h-screen left-0 top-0 ${display}`}>
-      <div className="flex justify-center mx-auto *:m-4 min-w-[50%] h-[50px] bg-red-700">
+      <div className="flex justify-center mx-auto *:m-4 min-w-[50%] h-[15%] bg-red-700">
         <p className="text-white">Error: {error}</p>
         <button className="hover:cursor-pointer hover:text-black text-white font-semibold" onClick={ onClick }>X</button>
       </div>
